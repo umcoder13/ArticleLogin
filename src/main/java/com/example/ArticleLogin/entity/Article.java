6641693be2f7ec22079ffb54ee0a58cac5d3783a
umcoder13 <68634885+umcoder13@users.noreply.github.com>
2022-01-31 23:44:43 +0900

    20220131 Entity 생성, 테이블 생성, Test Code 작성,

---
+++ src/main/java/com/example/ArticleLogin/entity/Article.java
@@ -0,0 +1,46 @@
+package com.example.ArticleLogin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDateTime;
+
+@Entity @Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, nullable = true)
+    @NotEmpty
+    private String userid;
+
+    @Column(length = 10, nullable = true)
+    @NotEmpty
+    private String nickname;
+
+    @Column(length = 30, nullable = true)
+    @NotEmpty
+    private String title;
+
+    @Column(nullable = true)
+    @NotEmpty
+    private String content;
+
+    @Column(nullable = true, updatable = false)
+    @CreatedDate
+    private LocalDateTime time;
+
+    @PrePersist
+    public void time() {
+        this.time = LocalDateTime.now();
+    }
+
+}
---
+++ src/test/java/com/example/ArticleLogin/repository/ArticleRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.example.ArticleLogin.repository;
+
+import com.example.ArticleLogin.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ArticleRepositoryTest {
+    @Autowired
+    ArticleRepository repository;
+
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testArticle() {
+        Article article = new Article();
+        article.setUserid("aaa");
+        article.setContent("hi");
+        article.setNickname("realA");
+        article.setTitle("Hello");
+
+        Article findArticle = repository.saveAndFlush(article);
+
+        assertEquals(findArticle.getId(), article.getId());
+        assertEquals(findArticle.getUserid(), article.getUserid());
+        assertEquals(findArticle, article);
+    }
+
+
+}
