9c6338096838905c893228d8045a15b8048f42ff
umcoder13 <68634885+umcoder13@users.noreply.github.com>
2022-01-31 22:08:36 +0900

    Initial commit

---
+++ src/main/java/com/example/ArticleLogin/ArticleLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArticleLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ArticleLogin/ArticleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ArticleLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
